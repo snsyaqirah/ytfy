e33f0fce2dc2f70592e711295c7857a243ea0108
Syaqi <snsyaqirah@gmail.com>
2025-10-16 11:56:44 +0800

    aaaaaaaaaaaaaaa

---
+++ utils/spotify_api.py
@@ -0,0 +1,5 @@
+def create_spotify_playlist(songs):
+    # Dummy placeholder for now
+    # Later: Use Spotify API to search + create playlist
+    print("Creating playlist with:", songs)
+    return "https://open.spotify.com/playlist/example"
---
+++ utils/youtube_api.py
@@ -0,0 +1,4 @@
+def get_youtube_songs(playlist_url):
+    # Dummy placeholder for now
+    # Later: Use YouTube Data API to fetch song titles + artists
+    return ["Song 1 - Artist", "Song 2 - Artist"]
